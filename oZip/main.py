c5dacb508080ef6b72beb72040ed8a3f7bae0dd2
OzTamir <TheOzTamir@gmail.com>
2014-08-28 19:20:35 +0300

    Added documentaion

--- oZip/main.py
+++ oZip/main.py
@@ -4,27 +4,51 @@ import os.path
 import sys
 
 def check_file(path):
+	''' Make sure that the file exists '''
 	if not os.path.exists(path):
 		raise ValueError('The path specified does not exists.')
 
 def choose_engine(path):
-	ext = os.path.splitext(path)
-	if ext == '.txt':
+	''' Choose a compression engine according to the file type '''
+	ext = path.split('.')
+	if ext[-1] == 'ozip':
+		ext = ext[-2]
+	else:
+		ext = ext[-1]
+	if ext == 'txt':
 		return TextCompressor()
 	return FilesCompressor()
 
 def main(path, decompress):
+	''' Main function '''
+	# Make sure the path is valid
 	try:
 		check_file(path)
 	except ValueError, e:
 		print str(e)
 		sys.exit(1)
-
+	# Get a compression engine usable with the input file
 	engine = choose_engine(path)
+	# Do the thing
 	if decompress:
-		engine.decompress(path)
+		try:
+			engine.decompress(path)
+			print 'Decompression done.'
+		except Exception, e:
+			print 'Something went wrong. Please issue a bug report on https://github.com/OzTamir/oZip/issues' + \
+				' with a screenshot of this error.'
+			print 'Error:\n%s' % str(e)
+			sys.exit(1)
 	else:
-		engine.compress(path)
+		try:
+			engine.compress(path)
+			print '\n'.join(['Compression done.', \
+							'Original file size: %s kb' % str(os.path.getsize(path) / 1024), \
+							'Compressed file size: %s kb' % str(os.path.getsize(path + '.ozip') / 1024)])
+		except Exception, e:
+			print 'Something went wrong. Please issue a bug report on https://github.com/OzTamir/oZip/issues' + \
+				' with a screenshot of this error.'
+			print 'Error:\n%s' % str(e)
+			sys.exit(1)
 
-	print 'Thanks for using oZip'
 	sys.exit(0)
