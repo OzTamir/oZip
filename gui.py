d74245df3bb7fbcbcfbfce768936e69ca401c3d6
OzTamir <TheOzTamir@gmail.com>
2014-08-29 21:25:38 +0300

    GUI seems to work

--- gui.py
+++ gui.py
@@ -1,9 +1,11 @@
 # GUI imports
-from Tkinter import Tk, Frame, BOTH, Checkbutton, IntVar, Listbox,END
+from Tkinter import Tk, Frame, BOTH, Checkbutton, Listbox,END
 import tkFileDialog 
 from ttk import Button, Style
 import tkMessageBox as box
 # Backend imports
+from Queue import Queue
+from threading import Thread
 from queue import runPool
 import ntpath
 
@@ -14,7 +16,9 @@ class MainWindow(Frame):
         self.parent = parent
         self.files = []
         self.lb = None
+        self.should_decompress = False
         self.initUI()
+        self.update_idletasks()
     
     def initUI(self):
         ''' Create the UI '''
@@ -25,39 +29,64 @@ class MainWindow(Frame):
         # 'Add File' button
         addFileButton = Button(self, text="Add File",
             command=self.onOpen)
-        addFileButton.place(x=250, y=50)
+        addFileButton.place(x=225, y=30)
         
         # 'Go' button
         goButton = Button(self, text="Go",
             command=self.onClick)
-        goButton.place(x=250, y=210)
+        goButton.place(x=225, y=100)
         
         # 'Quit' button
         quitButton = Button(self, text="Quit",
             command=self.quit)
-        quitButton.place(x=10, y=210)
+        quitButton.place(x=225, y=160)
         
         # Decompression checkbox
-        self.should_decompress = IntVar()
-        cb = Checkbutton(self, text="Should decompress",
-            variable=self.should_decompress)
-        cb.place(x=200, y=175)
+        self.cb = Checkbutton(self, text="Should decompress",
+            command=self.checkboxClick)
+        self.cb.place(x=20, y=195)
         
         # Selected files list
         lb = Listbox(self)
         self.lb = lb   
         lb.place(x=20, y=20)
 
+    def checkboxClick(self):
+        ''' Update the variable that stores the action that will be done on the files '''
+        # Update the GUI
+        self.cb.update_idletasks()
+        # Update the backend
+        self.should_decompress = not self.should_decompress
+
     def onClick(self):
         ''' 'Go' button pressed '''
-        # TODO: Display an error message
-        # Run the pool
-        runPool(self.files, bool(self.should_decompress))
-        # Show a message box
-        msg = 'Processing completed.'
-        box.showinfo("Done", msg)
+        # Create a queue for any error messages that may rise
+        err_q = Queue()
+        # Run the processing on another thread and use the current thread to display errors if occured
+        worker = Thread(target=runPool, args=(self.files, err_q, self.should_decompress))
+        worker.setDaemon(True)
+        worker.start()
+
+        # Display error messages if needed
+        while True:
+            # Get the next error message
+            var = err_q.get()
+            # If it's a tuple, display the error from the tuple
+            if isinstance(var, tuple):
+                self.showError(var[0], var[1])
+            # If it's a string it means that the processing is done
+            elif isinstance(var, str):
+                break
+            # Remove the error from the queue
+            err_q.task_done()
+        # When all the files are done, inform the user
+        box.showinfo("Done", 'Processing completed.')
         # Quit
         self.quit()
+
+    def showError(self, filename, error_msg):
+        ''' Show an error msg if needed '''
+        box.showerror("Error on file %s" % filename, error_msg)
 
     def onOpen(self):
         ''' 'Add File' button was clicked '''
@@ -73,7 +102,7 @@ class MainWindow(Frame):
 def main():
     ''' Main GUI Function '''
     root = Tk()
-    root.geometry("350x250+300+300")
+    root.geometry("350x225+300+300")
     app = MainWindow(root)
     root.mainloop()  
 
--- queue.py
+++ queue.py
@@ -5,32 +5,44 @@ import time
 # oZip Handler
 from oZip.main import main
 
-def Worker(i, q):
+def Worker(i, q, err_q, decompress):
 	''' A Worker thread to process a file '''
 	while True:
+		# Get the next file from the queue
 		filename = q.get()
-		main(filename, False)
+		# Run the compression on the file
+		res = main(filename, decompress)
+		# If the return value is a tuple, than there were errors
+		if isinstance(res, tuple):
+			# Report the error using the error queue
+			err_q.put(res)
+		# Report that the worker is ready for another file
 		q.task_done()
 
-def runPool(f_list, should_compress=False, pool_size=3):
+def runPool(f_list, err_q, should_compress=False, pool_size=5):
 	''' Create a thread pool to process a queue of files '''
 	# Don't create unemployed workers
 	if len(f_list) < pool_size:
 		pool_size = len(f_list)
+	# Initialize a queue with the file names
 	files_queue = Queue()
 	# Set up the threads pool
 	for i in range(pool_size):
-	    worker = Thread(target=Worker, args=(i, files_queue,))
+		# Start the workers
+	    worker = Thread(target=Worker, args=(i, files_queue, err_q, should_compress))
 	    worker.setDaemon(True)
 	    worker.start()
 
-	# Download the feed(s) and put the enclosure URLs into
-	# the queue.
+	# Add the files to the queue, from which the workers will pull new files
 	for filename in f_list:
 		files_queue.put(filename)
 	        
 	# Now wait for the queue to be empty (all files processed)
 	files_queue.join()
+	# Wait 'till all errors were reported
+	err_q.join()
+	# Add a message to signal that there aren't any other files waiting to be processed
+	err_q.put('Done')
 	return
 
 
